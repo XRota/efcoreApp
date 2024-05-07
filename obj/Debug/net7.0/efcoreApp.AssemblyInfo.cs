c8f9cde7fed82b11bd258f31c7a46f77488a36c1
İhsan Koray Semiz <119893221+XRota@users.noreply.github.com>
2024-05-07 11:41:42 +0300

    first upload

--- obj/Debug/net7.0/efcoreApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/efcoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9d7c52a41fc9470f2ebef295c2c4ae2fd28256a")]
 [assembly: System.Reflection.AssemblyProductAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
